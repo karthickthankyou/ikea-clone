b61137dfcd3e24337ce7f1f35db1b5764a3bb07a
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-03-23 22:33:26 +0530

    fix: made button look like a button with gray-900

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -3,7 +3,7 @@ export interface IButtonProps {}
 const Button = () => (
   <button
     type='button'
-    className='px-4 py-2 border border-black rounded shadow-lg'
+    className='px-4 py-2 border border-gray-900 rounded shadow-lg'
   >
     Click me
   </button>
