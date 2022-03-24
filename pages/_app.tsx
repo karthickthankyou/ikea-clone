6bf77a8a456db686c2dec810c1ad4defaf45c5eb
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-03-25 01:45:07 +0530

    feat: codegen with redux and urql

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import '../src/globals.css'
-import type { AppProps } from 'next/app'
+import { Provider as ReduxProvider } from 'react-redux'
+import UrqlProvider, {
+  ssrCache,
+} from 'src/components/atoms/UrqlProvider/UrqlProvider'
+import { store } from 'src/store'
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-)
+import type { AppProps } from 'next/app'
+import '../src/globals.css'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  if (pageProps.urqlState) {
+    ssrCache.restoreData(pageProps.urqlState)
+  }
+
+  return (
+    <ReduxProvider store={store}>
+      <UrqlProvider>
+        <Component {...pageProps} />
+      </UrqlProvider>
+    </ReduxProvider>
+  )
+}
 
 export default MyApp
