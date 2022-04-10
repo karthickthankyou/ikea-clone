5515bf492202732dcf89da71e7bd40597001356f
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-04-10 13:38:38 +0530

    fix: fixed auth header

--- lib/client.ts
+++ lib/client.ts
@@ -1,10 +1,12 @@
 import { createClient } from 'urql'
 
+const HASURA_ADMIN = process.env.HASURA_ADMIN || ''
+
 export const urqlAdminClient = createClient({
   url: 'https://ikea-karthick.herokuapp.com/v1/graphql',
   fetchOptions: {
     headers: {
-      Authorization: `bearer ${process.env.HASURA_ADMIN}`,
+      'x-hasura-admin-secret': HASURA_ADMIN,
     },
   },
 })
